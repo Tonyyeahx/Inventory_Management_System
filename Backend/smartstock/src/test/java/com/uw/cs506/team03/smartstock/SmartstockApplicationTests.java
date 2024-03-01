3a44a25eeb84cf74212ec14936ac65a3d2771375
jamesjaw <50652557+jamesjaw@users.noreply.github.com>
2024-03-01 15:21:39 -0600

    create a spring framwork

---
+++ Backend/smartstock/src/test/java/com/uw/cs506/team03/smartstock/SmartstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uw.cs506.team03.smartstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
