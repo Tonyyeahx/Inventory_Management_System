130f4b404ac67588d78e65056b0a056d0c5c8a22
Yucheng Zang <yzang7@wisc.edu>
2024-03-09 17:33:27 -0600

    FEAT: Implement The Side Menu Bar Without Links

--- Frontend/inventoryManagementSystem/README.md
+++ Frontend/inventoryManagementSystem/README.md
@@ -1,6 +1,11 @@
 ## External library installation
 
-React Pro Sidebar
+React Pro Sidebar (https://www.npmjs.com/package/react-pro-sidebar)
 ```
 npm install react-pro-sidebar
+```
+
+Material Design Icons (https://www.npmjs.com/package/mdi-react)
+```
+npm install mdi-react
 ```
--- Frontend/inventoryManagementSystem/src/components/MerchantNavbar.jsx
+++ Frontend/inventoryManagementSystem/src/components/MerchantNavbar.jsx
@@ -2,9 +2,18 @@
  * @file Created a side menubar / navbar for the merchant view users
  * @author Yucheng Zang <yzang7@wisc.edu>
  */
+
 // External imports
 import React from 'react'
 import { Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
+
+// Icon imports
+import StoreOutlineIcon from 'mdi-react/StoreOutlineIcon';
+import TruckCargoContainerIcon from 'mdi-react/TruckCargoContainerIcon';
+import CogIcon from 'mdi-react/CogIcon';
+import DevToIcon from 'mdi-react/DevToIcon';
+import LogoutIcon from 'mdi-react/LogoutIcon';
+import CartVariantIcon from 'mdi-react/CartVariantIcon';
 
 // Internal imports
 import teamLogo from "../assets/TeamLogo.png"
@@ -14,24 +23,31 @@ import "../App.css"
 import "./MerchantNavbar.css"
 
 function MerchantNavbar() {
-    return (
-        <Sidebar>
-            {/* Display project/team logo here in the top of the menu bar.
-                Used a logo wrapper to make it align at the center */}
-            <div className="logo-wrapper">
-                <img src={teamLogo} className="logo" alt="React logo" />
-            </div>
+  return ( 
+    // Used a div in order to extend the side menu bar from the top to the buttom of the screen
+    <div className="side-menu-bar">
 
-            <Menu>
-                <SubMenu label="Charts">
-                    <MenuItem> Pie charts </MenuItem>
-                    <MenuItem> Line charts </MenuItem>
-                </SubMenu>
-                <MenuItem> Documentation </MenuItem>
-                <MenuItem> Calendar </MenuItem>
-            </Menu>
-        </Sidebar>
-    );
+      <Sidebar backgroundColor='#ededed'>
+
+        {/* Display project/team logo here in the top of the menu bar. Used a logo wrapper to make
+            it align at the center */}
+        <div className="logo-wrapper">
+            <img src={teamLogo} className="logo" alt="React logo" />
+        </div>
+
+        {/* Start of the menu items */}
+          <Menu>
+              <MenuItem icon={<StoreOutlineIcon />}> Inventories </MenuItem>
+              <MenuItem icon={<TruckCargoContainerIcon />}> Orders </MenuItem>
+              <MenuItem icon={<DevToIcon />}> Menu 3 </MenuItem>
+              <MenuItem icon={<DevToIcon />}> Menu 4 </MenuItem>
+              <MenuItem icon={<CogIcon />}> Settings </MenuItem>
+              <MenuItem icon={<LogoutIcon />}> Logout </MenuItem>
+              <MenuItem icon={<CartVariantIcon />}> Consumer (Debug) </MenuItem>
+          </Menu>
+      </Sidebar>
+    </div>
+  );
 }
 
 export default MerchantNavbar
