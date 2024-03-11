3ada085e3f756f55f82bfb5fdac1297c43c727e9
Yucheng Zang <yzang7@wisc.edu>
2024-03-11 00:03:20 -0500

    FEAT: Add Placeholder Items to The Inventory Table

--- Frontend/inventoryManagementSystem/src/pages/Inventories/InventoriesTable.jsx
+++ Frontend/inventoryManagementSystem/src/pages/Inventories/InventoriesTable.jsx
@@ -13,7 +13,7 @@ import "../../App.css"
 
 function InventoriesTable() {
   return (
-    <Table striped bordered hover>
+    <Table bordered hover>
       {/* Table header */}
       <thead>
         <tr> 
@@ -27,6 +27,50 @@ function InventoriesTable() {
           <th>Supplier</th>
         </tr>
       </thead>
+
+      {/* Table body, with placeholder entries now, will be populated with map() and components */}
+      <tbody>
+      <tr> 
+          <td>Fresh Strawberries</td>
+          <td>Fresh Fruits</td>
+          <td>$3.99</td>
+          <td>$2.15</td>
+          <td>$0.99</td>
+          <td>200</td>
+          <td>02-01-2024</td>
+          <td>XYZ Farm</td>
+        </tr>
+        <tr> 
+          <td>Fried Chicken Tenders</td>
+          <td>Frozen</td>
+          <td>$2.50</td>
+          <td>$1.00</td>
+          <td>$0.00</td>
+          <td>114514</td>
+          <td>02-15-2024</td>
+          <td>iKun Warehouse</td>
+        </tr>
+        <tr> 
+          <td>Fried Basketball</td>
+          <td>Chinese Memes</td>
+          <td>$2.50</td>
+          <td>$1.00</td>
+          <td>$0.00</td>
+          <td>151085</td>
+          <td>02-15-2024</td>
+          <td>iKun Warehouse</td>
+        </tr>
+        <tr> 
+          <td>Jin Ke La</td>
+          <td>Chinese Memes</td>
+          <td>$18.00</td>
+          <td>$10.00</td>
+          <td>$0.00</td>
+          <td>1800</td>
+          <td>02-15-2024</td>
+          <td>Bilibili</td>
+        </tr>
+      </tbody>
     </Table>
   );
 
