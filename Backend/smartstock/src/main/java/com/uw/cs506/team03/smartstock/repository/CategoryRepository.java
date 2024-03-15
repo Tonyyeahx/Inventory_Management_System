73c0204a718e156bb195db1448a503766d1a9a1d
ZIHAO YE <zye83@wisc.edu>
2024-03-15 13:59:02 -0500

    FIX: added @Repository annotation for all repository .java classes

--- Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/repository/CategoryRepository.java
+++ Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/repository/CategoryRepository.java
@@ -4,6 +4,7 @@ import com.uw.cs506.team03.smartstock.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 // This interface is used to create sql searching methods for the Category object
 public interface CategoryRepository extends JpaRepository<Category, Integer>{
     Category findByCategoryId(int categoryId); // search by category id
--- Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/repository/ProductRepository.java
+++ Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.uw.cs506.team03.smartstock.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 // This interface is used to create sql searching methods for the Product object
 public interface ProductRepository extends JpaRepository<Product, Integer>{
     Product findByProductId(int productId); // search by product id
