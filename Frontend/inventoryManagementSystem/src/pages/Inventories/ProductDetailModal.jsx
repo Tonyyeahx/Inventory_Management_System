49681610c910f1de33e7a673e675ac90b900c6dc
Yucheng Zang <yzang7@wisc.edu>
2024-03-14 22:45:25 -0500

    FEAT: Able to Show Product Details in Product Detail Modal

--- Frontend/inventoryManagementSystem/src/pages/Inventories/ProductDetailModal.jsx
+++ Frontend/inventoryManagementSystem/src/pages/Inventories/ProductDetailModal.jsx
@@ -7,6 +7,7 @@
 import React, {useState} from 'react'
 import Modal from 'react-bootstrap/Modal';
 import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Image } from 'react-bootstrap';
@@ -33,8 +34,82 @@ function ProductDetailModal(props) {
             <Col className="d-flex justify-content-center">
               <Image fluid src={props.displayContent.productImg}/>
             </Col>
-            <Col>
-              hello right
+
+            <Col className="d-flex justify-content-center">
+              {/* Use a table to show the product's detail */}
+              <Table striped borderless hover>
+                <tbody>
+                  <tr>
+                    <th>Product Name</th>
+                    <td>{props.displayContent.productName}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Product ID</th>
+                    <td>{props.displayContent.productID}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Inventory ID</th>
+                    <td>{props.displayContent.inventoryID}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Store ID</th>
+                    <td>{props.displayContent.storeID}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Category</th>
+                    <td>{props.displayContent.category}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Category ID</th>
+                    <td>{props.displayContent.categoryID}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Cost</th>
+                    <td>{props.displayContent.cost}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Sell Price</th>
+                    <td>{props.displayContent.sellPrice}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Discount</th>
+                    <td>{props.displayContent.discount}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Remaining Quantity</th>
+                    <td>{props.displayContent.quantity}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Ordered Quantity</th>
+                    <td>{props.displayContent.orderedQuantity}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Last Ordered Date</th>
+                    <td>{props.displayContent.lastOrderedDate}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Supplier</th>
+                    <td>{props.displayContent.supplier}</td>
+                  </tr>
+
+                  <tr>
+                    <th>Supplier ID</th>
+                    <td>{props.displayContent.supplierID}</td>
+                  </tr>
+                </tbody>
+              </Table>
             </Col>
           </Row>
         </Container>
