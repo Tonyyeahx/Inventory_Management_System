de3dbbdd102534eba364f697d388fda523b37a78
Yucheng Zang <yzang7@wisc.edu>
2024-03-30 17:43:38 -0500

    FEAT: Implement A Radio Button Switching View/Edit Mode

---
+++ Frontend/inventoryManagementSystem/src/components/ViewEditModeRadioButton.jsx
@@ -0,0 +1,52 @@
+/**
+ * @file A radio button used in the Inventory Page to switch between 
+ * @author Yucheng Zang <yzang7@wisc.edu>
+ */
+
+// External imports
+import React, { useState, useEffect, useContext } from 'react'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import ToggleButton from 'react-bootstrap/ToggleButton'
+
+// Internal imports
+
+// Context imports
+import ViewModeContext from '../context/ViewModeContext'
+
+// CSS imports
+import "../App.css"
+
+function ViewEditModeRadioButton() {
+  const [viewEditMode, setViewEditMode] = useContext(ViewModeContext)
+
+  return (
+    // The 'w-100' is a React-Bootstrap utility class making the element to take up the full width 
+    // of their parent container
+    <ButtonGroup className="w-100" size='sm'>
+      <ToggleButton 
+        id="view-radio-button"
+        type='radio'
+        variant='outline-secondary'
+        checked={viewEditMode == "View"}
+        onClick={() => setViewEditMode("View")}
+      >
+        View
+      </ToggleButton>
+
+      <ToggleButton 
+        id="edit-radio-button"
+        type='radio'
+        variant='outline-danger'
+        checked={viewEditMode == "Edit"}
+        onClick={() => setViewEditMode("Edit")}
+      >
+        Edit
+      </ToggleButton>
+
+    </ButtonGroup>
+  );
+}
+
+
+export default ViewEditModeRadioButton
+
