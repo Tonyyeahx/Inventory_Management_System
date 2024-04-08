eabf1911482c908c1acb2692789e1ed7db5c9a32
jamesjaw <yjaw@wisc.edu>
2024-04-08 01:19:21 -0500

    REFACTOR: Supplier APIs

--- Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/service/SupplierService.java
+++ Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/service/SupplierService.java
@@ -1,5 +1,16 @@
 package com.uw.cs506.team03.smartstock.service;
 
+import com.uw.cs506.team03.smartstock.entity.Category;
+import com.uw.cs506.team03.smartstock.entity.Supplier;
+
+import java.util.List;
+
 public interface SupplierService {
-    
+    public List<Supplier> findAll();
+
+    public Supplier findById(int id);
+
+    public Supplier save(Supplier supplier);
+
+    public void deleteById(int id);
 }
