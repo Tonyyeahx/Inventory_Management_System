ab045390616ea62ca3c214066325681072768dfc
ZIHAO YE <zye83@wisc.edu>
2024-04-25 20:17:49 -0500

    REFACTOR: added javadoc plugin to pom.xml

--- Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/entity/Category.java
+++ Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/entity/Category.java
@@ -6,17 +6,30 @@ import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 
+/**
+ * This class is the entity for the category table
+ */
 @Entity
 @Table(name = "category")
 public class Category {
+
+    /**
+     * This is the id of the category
+     */
     @Id // pk
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
     private int categoryId;
 
+    /**
+     * This is the name of the category
+     */
     @Column(name = "category_name", nullable = false)
     private String categoryName;
 
+    /**
+     * This is the list of products in the category
+     */
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Product> products;
 
@@ -25,35 +38,68 @@ public class Category {
 
     }
 
+    /**
+     * This is the constructor for the Category class
+     * @param categoryName the name of the category
+     */
     public Category(String categoryName) {
         this.categoryName = categoryName;
         this.products = new ArrayList<>();
     }
 
+    /**
+     * This method is used to get the products
+     * @return the products
+     */
     public List<Product> getProducts() {
         return products;
     }
 
+    /**
+     * This method is used to set the products
+     * @param products the products
+     */
     public void setProducts(List<Product> products) {
         this.products = products;
     }
 
+    /**
+     * This method is used to add a product to the category
+     * @param product the product
+     */
     public int getCategoryId() {
         return categoryId;
     }
 
+    /**
+     * This method is used to set the id of the category
+     * @param categoryId the id of the category
+     */
     public void setCategoryId(int categoryId) {
         this.categoryId = categoryId;
     }
 
+    /**
+     * This method is used to get the name of the category
+     * @return the name of the category
+     */
     public String getCategoryName() {
         return categoryName;
     }
 
+    /**
+     * This method is used to set the name of the category
+     * @param categoryName the name of the category
+     */
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
     }
 
+    /**
+     * This method is used to add a product to the category
+     * @param product the product
+     */
+    @Override
     public String toString() {
         return "Category{" +
                 "categoryId=" + categoryId +
--- Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/entity/Inventory.java
+++ Backend/smartstock/src/main/java/com/uw/cs506/team03/smartstock/entity/Inventory.java
@@ -8,45 +8,78 @@ import java.util.Date;
 @Entity
 @Table(name = "inventory")
 
-// This class is used to create an object that represents the inventory table in the database
+/**
+ * This class is the entity for the inventory table
+ */
 public class Inventory{
     @Id // pk
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    // Attributes for Inventory object
+    /**
+     * This is the id of the inventory
+     */
     @Column(name = "inventory_id", nullable = false)
     private int inventoryId;
 
+    /**
+     * This is the quantity of the product in the inventory
+     */
     @Column(name = "quantity", nullable = false)
     private int quantity;
 
+    /**
+     * This is the last order date of the product in the inventory
+     */
     @Column(name = "last_order_date", nullable = false)
     private Date lastOrderDate;
 
+    /**
+     * This is the order quantity of the product in the inventory
+     */
     @Column(name = "order_quantity", nullable = false)
     private int orderQuantity;
 
+    /**
+     * This is the discount of the product in the inventory
+     */
     @Column(name = "discount", nullable = false)
     private float discount;
 
+    /**
+     * This is the sell price of the product in the inventory
+     */
     @Column(name = "sell_price", nullable = false)
     private float sellPrice;
 
+    /**
+     * This is the product in the inventory
+     */
     @ManyToOne
     @JoinColumn(name = "product_id")
     @JsonIgnore
     private Product product;
 
+    /**
+     * This is the store of the inventory
+     */
     @ManyToOne
     @JoinColumn(name = "store_id")
     @JsonIgnore
     private Store store;
-    // Default constructor for Inventory object
+
     public Inventory() {
 
     }
     
-    // Constructor for Inventory object
+    /**
+     * This is the constructor for the Inventory class
+     * @param inventoryId the id of the inventory
+     * @param quantity the quantity of the product in the inventory
+     * @param lastOrderDate the last order date of the product in the inventory
+     * @param orderQuantity the order quantity of the product in the inventory
+     * @param discount the discount of the product in the inventory
+     * @param sellPrice the sell price of the product in the inventory
+     */
     public Inventory(int inventoryId, int storeId, int productId, int quantity, Date lastOrderDate, int orderQuantity, float discount, float sellPrice) {
         this.inventoryId = inventoryId;
         this.quantity = quantity;
@@ -56,80 +89,147 @@ public class Inventory{
         this.sellPrice = sellPrice;
     }
 
-    // Getters and Setters for Inventory object
+    /**
+     * This method is used to get the id of the inventory
+     * @return the id of the inventory
+     */
     public int getInventoryId() {
         return inventoryId;
     }
 
+    /**
+     * This method is used to set the id of the inventory
+     * @param inventoryId the id of the inventory
+     */
     public void setInventoryId(int inventoryId) {
         this.inventoryId = inventoryId;
     }
 
+    /**
+     * This method is used to get the quantity of the product in the inventory
+     * @return the quantity of the product in the inventory
+     */
     public int getQuantity() {
         return quantity;
     }
 
+    /**
+     * This method is used to set the quantity of the product in the inventory
+     * @param quantity the quantity of the product in the inventory
+     */
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
+    /**
+     * This method is used to get the last order date of the product in the inventory
+     * @return the last order date of the product in the inventory
+     */
     public Date getLastOrderDate() {
         return lastOrderDate;
     }
 
+    /**
+     * This method is used to set the last order date of the product in the inventory
+     * @param lastOrderDate the last order date of the product in the inventory
+     */
     public void setLastOrderDate(Date lastOrderDate) {
         this.lastOrderDate = lastOrderDate;
     }
 
+    /**
+     * This method is used to get the order quantity of the product in the inventory
+     * @return the order quantity of the product in the inventory
+     */
     public int getOrderQuantity() {
         return orderQuantity;
     }
 
+    /**
+     * This method is used to set the order quantity of the product in the inventory
+     * @param orderQuantity the order quantity of the product in the inventory
+     */
     public void setOrderQuantity(int orderQuantity) {
         this.orderQuantity = orderQuantity;
     }
 
+    /**
+     * This method is used to get the discount of the product in the inventory
+     * @return the discount of the product in the inventory
+     */
     public float getDiscount() {
         return discount;
     }
 
+    /**
+     * This method is used to set the discount of the product in the inventory
+     * @param discount the discount of the product in the inventory
+     */
     public void setDiscount(float discount) {
         this.discount = discount;
     }
 
+    /**
+     * This method is used to get the sell price of the product in the inventory
+     * @return the sell price of the product in the inventory
+     */
     public float getSellPrice() {
         return sellPrice;
     }
 
+    /**
+     * This method is used to set the sell price of the product in the inventory
+     * @param sellPrice the sell price of the product in the inventory
+     */
     public void setSellPrice(float sellPrice) {
         this.sellPrice = sellPrice;
     }
 
+    /**
+     * This method is used to get the product in the inventory
+     * @return product the product in the inventory
+     */
     public Product getProduct() {
         return product;
     }
 
+    /**
+     * This method is used to set the product in the inventory
+     * @param product the product in the inventory
+     */
     public void setProduct(Product product) {
         this.product = product;
     }
 
+    /**
+     * This method is used to get the store of the inventory
+     * @return store the store of the inventory
+     */
     public Store getStore() {
         return store;
     }
 
+    /**
+     * This method is used to set the store of the inventory
+     * @param store the store of the inventory
+     */
     public void setStore(Store store) {
         this.store = store;
     }
 
-
-
-    // Method to add quantity to a product within the inventory
+    /**
+     * This method is used to add quantity to the product in the inventory
+     * @param quantityToAdd the quantity to add to the product in the inventory
+     */
     public void addQuantity(int quantityToAdd) {
         this.quantity += quantityToAdd;
     }
 
-    // Override toString method for Inventory object
     @Override
+    /**
+     * This method is used to get the string representation of the Inventory object
+     * @return the string representation of the Inventory object
+     */
     public String toString() {
         return "Inventory{" +
                 "inventory_id=" + inventoryId +
